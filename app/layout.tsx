8dfa031a6c112470aa69ed1304d4d18d4fc41436
Somaan07 <5164somaan@gmail.com>
2026-06-09 19:50:26 -0400

    fixed typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "T-rex Window & Eaves Cleaning INC.",
+  title: "T-Rex Window & Eaves Cleaning INC.",
   description: "Serving The GTA Since 2007",
 };
 
